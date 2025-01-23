c07b8ca6d816893533cddb31a16dfc2ecec4c4f2
Douglas Reder <dreder@macbooktoo.local>
2025-01-23 14:41:23 -0600

    first commit

---
+++ quai-gpu-miner/build/kawpowminer/CMakeFiles/kawpowminer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kawpowminer.dir/main.cpp.o"
+  "kawpowminer"
+  "kawpowminer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kawpowminer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
